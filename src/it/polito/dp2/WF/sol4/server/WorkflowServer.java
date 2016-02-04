b7da86e402c56ca0b9ea2b9a3b5a2580fca3980d
S200124 <S200124@studenti.polito.it>
2016-02-04 18:05:58 +0100

    Commit 18:05

---
+++ src/it/polito/dp2/WF/sol4/server/WorkflowServer.java
@@ -0,0 +1,14 @@
+package it.polito.dp2.WF.sol4.server;
+
+import javax.xml.ws.Endpoint;
+
+public class WorkflowServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
